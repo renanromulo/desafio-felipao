93f2ec1720f9b7660d694e5ea8bbe60512967c8c
renanromulo <renanromulo18@gmail.com>
2026-05-05 11:19:47 -0300

    Terminando o desafio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("Olá Mundo!");
+let nickName = "dentinho";
+let level = 11312;
+
+
+if (level <= 1000) {
+    
+    console.log(`O herói de nome ${nickName} está no nível de Ferro!`);
+}
+if (level > 1001 && level <= 2000) {
+    
+    console.log(`O herói de nome ${nickName} está no nível de Bronze!`);
+}
+if (level > 2001 && level <= 5000) {
+    
+    console.log(`O herói de nome ${nickName} está no nível de Prata!`);
+}
+if (level > 5001 && level <= 7000) {
+    
+    console.log(`O herói de nome ${nickName} está no nível de Ouro!`);
+}
+if (level > 7001 && level <= 8000) {
+    
+    console.log(`O herói de nome ${nickName} está no nível de Platina!`);
+}
+if (level > 8001 && level <= 9000) {
+    
+    console.log(`O herói de nome ${nickName} está no nível de Ascendente!`);
+}
+if (level > 9001 && level <= 10000) {
+    
+    console.log(`O herói de nome ${nickName} está no nível de Imortal!`);
+}
+else if (level >= "10001") {
+    
+    console.log(`O herói de nome ${nickName} está no nível de Radiante!`);
+}
