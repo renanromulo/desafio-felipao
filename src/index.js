3f721b42415d451eca41e27790a7b8109afdc128
renanromulo <renanromulo18@gmail.com>
2026-05-05 11:48:13 -0300

    Alterado o modo if para swithc no codigo

--- src/index.js
+++ src/index.js
@@ -2,35 +2,29 @@ let nickName = "dentinho";
 let level = 11312;
 
 
-if (level <= 1000) {
-    
+switch (level) {
+  case (level <= 1000):
     console.log(`O herói de nome ${nickName} está no nível de Ferro!`);
-}
-if (level > 1001 && level <= 2000) {
-    
+    break;
+  case (level > 1000 && level <= 2000):
     console.log(`O herói de nome ${nickName} está no nível de Bronze!`);
-}
-if (level > 2001 && level <= 5000) {
-    
+    break;
+  case (level > 2001 && level <= 5000):
     console.log(`O herói de nome ${nickName} está no nível de Prata!`);
-}
-if (level > 5001 && level <= 7000) {
-    
+    break;
+  case (level > 5001 && level <= 7000):
     console.log(`O herói de nome ${nickName} está no nível de Ouro!`);
-}
-if (level > 7001 && level <= 8000) {
-    
+    break;
+  case (level > 7001 && level <= 8000):
     console.log(`O herói de nome ${nickName} está no nível de Platina!`);
-}
-if (level > 8001 && level <= 9000) {
-    
+    break;
+  case (level > 8001 && level <= 9000):
     console.log(`O herói de nome ${nickName} está no nível de Ascendente!`);
-}
-if (level > 9001 && level <= 10000) {
-    
+    break;
+  case (level > 9001 && level <= 10000):
     console.log(`O herói de nome ${nickName} está no nível de Imortal!`);
+    break;
+  default:
+     console.log(`O herói de nome ${nickName} está no nível de Radiante!`);
 }
-else if (level >= "10001") {
-    
-    console.log(`O herói de nome ${nickName} está no nível de Radiante!`);
-}
+
